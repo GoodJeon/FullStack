44bc13fb650e950fb0c57184482a9f19cf003e93
Jeon Dong Jun <513ehdwnsl@gmail.com>
2022-02-09 02:15:49 +0900

    django day4 second

---
+++ Mulcam/django/updown/updown/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.core.files.base import ContentFile
+from django.core.files.storage import default_storage
+from django.http import HttpResponse
+
+def index(request):
+    return render(request, 'index.html')
+
+def upload_process(request):
+    upload_file = request.FILES['uploadfile']
+
+
+    uploaded = default_storage.save(upload_file.name, ContentFile(upload_file.read()))
+
+
+    return render(request, 'download.html', {'filename': uploaded})
+
+
+def download_process(request, filename):
+    response = HttpResponse(default_storage.open(filename).read())
+    response['Content-Disposition'] = f"attachment; filename={filename}"
+
+    return response
