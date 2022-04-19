43d66ff9c54a79a21954367350ecc3ad7d24296a
Jeon Dong Jun <513ehdwnsl@gmail.com>
2022-04-20 00:23:06 +0900

    파이널프로젝트_망고플레이트_리뷰크롤링 0~1136

--- Mulcam/final_team4/test.py
+++ Mulcam/final_team4/test.py
@@ -1,14 +1,7 @@
-store = {}
-store['s_menu'] = []
-split_menus = ['수원LA왕갈비 (250g)', '25,000원', '수원양념갈비 (230g)', '21,000원', '생안창살 (150g)', '21,000원', '청기와토장찌개', '6,000원', '냉면 (물/비빔)', '6,000원']
-for i in range(0, len(split_menus), 2):
-    menu = {}
-    print(split_menus[i])
-    name = split_menus[i]
-    price = split_menus[i+1]
-    menu[name] = price
-    store['s_menu'].append(menu)
-    print(i)
+import datetime
 
-
-print(store)
+dt_now = datetime.datetime.now()
+current = dt_now.date()
+i = 3
+re_date = current - datetime.timedelta(days=i)
+print(re_date)
